5fd0e4d63726ad2df0dab3996769da3a1d574234
Karmen <katgucilatar@outlook.com>
2023-08-30 23:32:55 -0400

    created About Me page and Error page as well as added pathways for all the pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+export default function About() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <img src="./assets/kat.jpg"/>
+        <p>Hello there! I'm Katrina Gucilatar, a passionate individual with a diverse range of interests and experiences. As a dedicated mother to both a bundle of joy and some adorable fur babies, my heart finds joy in nurturing and caring for those around me. </p>
+
+        <p>With a background in linguistics, I've always been fascinated by the intricacies of communication and expression. However, life had an intriguing twist in store for me when I found myself drawn into the world of web development. It was a leap that I took with determination, enrolling in the Full Stack Web Development Coding Bootcamp at UNC Chapel Hill. This journey has been a thrilling roller coaster, filled with challenges that have only fueled my appetite for learning and growth.</p>
+
+        <p>One of my biggest passions is exploring the world, both through travel and through the culinary arts. There's something magical about discovering new places, cultures, and flavors – each experience leaves an indelible mark on my soul. In the realm of psychology, I find endless fascination in understanding the intricacies of the human mind and behavior, a curiosity that enriches both my personal and professional pursuits.</p>
+
+        <p>As I stand on the cusp of the future, I have my sights set on a software engineering apprenticeship. The prospect of honing my skills further and contributing to innovative technological solutions excites me beyond words. Every step of this journey has brought me to where I am today, and I'm eager to embrace every opportunity that comes my way.</p>
+
+        <p>Join me as I navigate this dynamic and ever-evolving adventure, fueled by my love for coding, my family, and the wonders of life itself. Thank you for being a part of my story!</p>
+
+        <p>Warm regards,
+        <br/>
+        Katrina Gucilatar</p>
+      </div>
+    );
+  }
