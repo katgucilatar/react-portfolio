e9f45da378e0a85c43e123324fbf46f2772f49e6
Karmen <katgucilatar@outlook.com>
2023-08-30 22:54:26 -0400

    first commit, installed vite, added components and pages folders/files, and created Header and Navigation

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Nav from './components/Nav';
+
+function Header() {
+    return (
+        <>
+        <h1>Katrina Gucilatar</h1>
+        <Nav />
+        </>
+    );
+}
+
+export default Header;
